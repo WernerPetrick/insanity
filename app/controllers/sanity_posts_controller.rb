184d3fd993c5cc90c6c6061e72f0835c1891b692
WernerPetrick <128157845+WernerPetrick@users.noreply.github.com>
2024-10-03 13:17:57 +0200

    Added functionality to create Sanity Posts

--- app/controllers/sanity_posts_controller.rb
+++ app/controllers/sanity_posts_controller.rb
@@ -12,4 +12,44 @@ class SanityPostsController < ApplicationController
     @post = response['result'].first if response
   end
   
+  def new
+  end
+  
+  def create
+    Rails.logger.debug("Received parameters: #{params.inspect}")
+    document_params = {
+      _type: 'post',
+      title: params[:title],
+      slug: {
+      _type: 'slug',
+      current: params[:slug]
+    },
+    body: [
+      {
+        _type: 'block',
+        children: [
+          {
+            _type: 'span',
+            text: params[:body]
+          }
+        ]
+      }
+    ]}
+
+    response = SanityPostsHelper.create_document(document_params)
+    
+    if response && response['results']
+      flash[:notice] = 'Post created successfully!'
+      redirect_to sanity_posts_path
+    else
+      flash[:alert] = "Failed to create post: #{response.inspect}"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params_from_request
+    params.require(:post).permit(:body, :title)
+  end
 end
--- app/helpers/sanity_posts_helper.rb
+++ app/helpers/sanity_posts_helper.rb
@@ -1,13 +1,44 @@
 module SanityPostsHelper
   
   BASE_URL = "https://#{ENV['SANITY_PROJECT_ID']}.api.sanity.io/v#{ENV['SANITY_API_VERSION']}/data/query/#{ENV['SANITY_DATASET']}"
-
-  def self.fetch(query)
-    response = HTTP.get("#{BASE_URL}?query=#{query}")
+  SANITY_API_URL = "https://#{ENV['SANITY_PROJECT_ID']}.api.sanity.io/v#{ENV['SANITY_API_VERSION']}/data/mutate/#{ENV['SANITY_DATASET']}"
+  SANITY_TOKEN = ENV['SANITY_TOKEN'] 
+  
+  def self.fetch(query, params = {})
+    if params.any?
+      params.each do |key, value|
+        query = query.gsub("$#{key}", value.to_s)
+      end
+    end
+    
+    encoded_query = URI.encode_www_form_component(query)
+    response = HTTP.get("#{BASE_URL}?query=#{encoded_query}")
     JSON.parse(response.body.to_s)
   rescue HTTP::Error => e
     Rails.logger.error("Sanity API error: #{e.message}")
     nil
   end
 
+  def self.create_document(document_data)
+    mutation = {
+      mutations: [
+        {
+          create: document_data
+        }
+      ]
+    }
+
+    response = HTTP.auth("Bearer #{SANITY_TOKEN}")
+                   .post(SANITY_API_URL, json: mutation)
+
+    if response.status.success?
+      JSON.parse(response.body.to_s)
+    else
+      Rails.logger.error("Failed to create document: #{response.body.to_s}")
+      nil
+    end
+  rescue HTTP::Error => e
+    Rails.logger.error("Sanity API error: #{e.message}")
+    nil
+  end
 end
